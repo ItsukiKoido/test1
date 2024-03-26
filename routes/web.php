0be4c125b485cd8bac479383c079035f766ee086
ItsukiKoido <163612212+ItsukiKoido@users.noreply.github.com>
2024-03-26 12:14:25 +0900

    Revert "PRタイトル"

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-
-
-Route::get('/posts', [PostController::class, 'index']);   
