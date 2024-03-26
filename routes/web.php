ecc1701afdba8ac61dd969137f1c17594af108f0
ItsukiKoido <af22080@shibaura-it.ac.jp>
2024-03-26 18:09:30 +0900

    カリキュラム8-2終了

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
 
 Route::get('/posts', [PostController::class, 'index']);   
