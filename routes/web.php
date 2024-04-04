d9f59c5f324ccbf3d0607f25228836adf6303b2d
ItsukiKoido <af22080@shibaura-it.ac.jp>
2024-04-04 15:20:49 +0900

    delete機能追加

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
